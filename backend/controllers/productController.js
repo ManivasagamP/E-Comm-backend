f605d9465caf8e4ac049c1ff501e70971fdb9804
ManivasagamP <manip27cse@gmail.com>
2024-08-05 03:47:56 -0700

    colpleted the product api functions

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,7 +21,7 @@ exports.newProduct = async(req,res,next)=>{
     })
 };
 
-//Get single Product
+//Get single Product - /api/v1/product/:id
 exports.getSingleProduct = async(req,res,next)=>{
     const product = await Product.findById(req.params.id);
     if(!product){
@@ -35,3 +35,46 @@ exports.getSingleProduct = async(req,res,next)=>{
         product 
     })
 };
+
+//Update Product- /api/v1/product/:id
+
+exports.updateProduct = async(req,res,next)=>{
+    let product= await Product.findById(req.params.id)
+
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message: "Product not found"     
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new :true,
+        runValidators:true
+    })
+
+    res.status(200).json({
+        success:true,
+        product   
+    });
+}
+
+//Delete Product- /api/v1/product/:id
+
+exports.deleteProduct = async(req,res,next)=>{
+    const product= await Product.findById(req.params.id)
+
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message: "Product not found"     
+        });
+    }
+    
+    await product.deleteOne();
+
+    res.status(200).json({
+        success:true,
+        message: "Product deleted"
+    })
+}
