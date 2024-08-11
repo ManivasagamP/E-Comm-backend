31fdf800c01304987e7a8742aa66543eed56bf69
ManivasagamP <manip27cse@gmail.com>
2024-08-11 08:02:56 -0700

    React dependencies installed home setup is done

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Home from './components/Home';
+import Footer from './components/Layouts/Footer';
+import Header from './components/Layouts/Header';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { HelmetProvider} from 'react-helmet-async';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <HelmetProvider>
+          <Header />
+            <Routes>
+              <Route path='/' element={<Home />} />
+            </Routes>
+          <Footer />
+        </HelmetProvider>
+      </div>
+    </Router>
+    
+  );
+}
+
+
+export default App;
